e5f1017f0d2b1f61c630a55ec5d216176c347a25
Pablo Ferraris <pablo.ferraris@outlook.com>
2016-06-07 16:56:54 -0300

    Métodos POST/DELETE para Listeners y Modules

--- LinkupSharp.Management/Controllers/ListenersController.cs
+++ LinkupSharp.Management/Controllers/ListenersController.cs
@@ -1,4 +1,6 @@
 ﻿using LinkupSharp.Channels;
+using LinkupSharp.Serializers;
+using System;
 using System.Linq;
 using System.Web.Http;
 
@@ -24,6 +26,53 @@ namespace LinkupSharp.Management.Controllers
         public IHttpActionResult Available()
         {
             return Ok(DependencyHelper.GetClasses<IChannelListener>().Select(x => x.Name.Replace("`1", "")).ToArray());
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Post([FromBody]ListenerDefinition definition)
+        {
+            try
+            {
+                if (Management.Server.Listeners.Any(x => x.Endpoint.Equals(definition.Endpoint, StringComparison.InvariantCultureIgnoreCase)))
+                    return BadRequest("Endpoint in use yet");
+                var type = DependencyHelper.GetClasses<IChannelListener>().FirstOrDefault(x => x.Name.Replace("`1", "").Equals(definition.Type, StringComparison.InvariantCultureIgnoreCase));
+                if (type == null)
+                    return BadRequest("Listener type not found");
+                var genericType = type.MakeGenericType(typeof(JsonPacketSerializer));
+                var listener = Activator.CreateInstance(genericType) as IChannelListener;
+                listener.Endpoint = definition.Endpoint;
+                Management.Server.AddListener(listener);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public IHttpActionResult Delete([FromBody]ListenerDefinition definition)
+        {
+            try
+            {
+                var listener = Management.Server.Listeners.FirstOrDefault(x => x.Endpoint.Equals(definition.Endpoint, StringComparison.InvariantCultureIgnoreCase));
+                if (listener == null)
+                    return BadRequest("Endpoint not found");
+                Management.Server.RemoveListener(listener);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        public class ListenerDefinition
+        {
+            public string Type { get; set; }
+            public string Endpoint { get; set; }
         }
     }
 }
--- LinkupSharp.Management/Controllers/ModulesController.cs
+++ LinkupSharp.Management/Controllers/ModulesController.cs
@@ -1,4 +1,5 @@
 ﻿using LinkupSharp.Modules;
+using System;
 using System.Linq;
 using System.Web.Http;
 
@@ -22,6 +23,45 @@ namespace LinkupSharp.Management.Controllers
         public IHttpActionResult Available()
         {
             return Ok(DependencyHelper.GetClasses<IServerModule>().Select(x => x.Name.Replace("`1", "")).ToArray());
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IHttpActionResult Post([FromBody]string typeName)
+        {
+            try
+            {
+                if (Management.Server.Modules.Any(x => x.GetType().Name.Equals(typeName, StringComparison.InvariantCultureIgnoreCase)))
+                    return BadRequest("Module is added yet");
+                var type = DependencyHelper.GetClasses<IServerModule>().FirstOrDefault(x => x.Name.Equals(typeName, StringComparison.InvariantCultureIgnoreCase));
+                if (type == null)
+                    return BadRequest("Listener type not found");
+                var module = Activator.CreateInstance(type) as IServerModule;
+                Management.Server.AddModule(module);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
+        }
+
+        [HttpDelete]
+        [Route("")]
+        public IHttpActionResult Delete([FromBody]string typeName)
+        {
+            try
+            {
+                var module = Management.Server.Modules.FirstOrDefault(x => x.GetType().Name.Equals(typeName, StringComparison.InvariantCultureIgnoreCase));
+                if (module == null)
+                    return BadRequest("Module not found");
+                Management.Server.RemoveModule(module);
+                return Ok();
+            }
+            catch (Exception ex)
+            {
+                return InternalServerError(ex);
+            }
         }
     }
 }
